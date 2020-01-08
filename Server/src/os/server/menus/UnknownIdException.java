73c0b1c6174475fe881ad00339752a96427ff2a2
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-01-08 19:12:58 +0000

    Update UnknownIdException.java

--- Server/src/os/server/menus/UnknownIdException.java
+++ Server/src/os/server/menus/UnknownIdException.java
@@ -1,7 +1,7 @@
 package os.server.menus;
 
 /**
- *	Thrown to indicate the user had entered an ID for a User/Agent/Player that doesn't exist.
+ *	Thrown to indicate the user has entered an ID for a User/Agent/Player that does not exist.
  */
 public class UnknownIdException extends RuntimeException {
 	private static final long serialVersionUID = 5837173913693231717L;
